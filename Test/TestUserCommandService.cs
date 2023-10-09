b9ba2bba62ca7551003ef229d0823378ac0ad775
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2023-10-09 21:05:51 +0300

    added mockups

---
+++ Test/TestUserCommandService.cs
@@ -0,0 +1,35 @@
+﻿using ClinicScheduler.exceptii;
+using ClinicScheduler.user.model;
+using ClinicScheduler.user.repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace Test
+{
+    public class TestUserCommandService
+    {
+        
+
+        [Fact]
+        public void TestAdd()
+        {
+            IUserRepository repo = new UserRepository();
+
+            User user = new User()
+                .setNume("Bob")
+                .setParola("parolaX")
+                .setTip(1);
+
+            repo.Add(user);
+
+            Assert.Contains(user, repo.GetAllUsers());
+            
+        }
+
+    }
+}
